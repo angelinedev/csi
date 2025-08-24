7cb6d9170580d6aff2d7cf8c020ef908a20c96fb
mahendra.cse2022 JCE <mahendra.cse2022@jerusalemengg.ac.in>
2025-08-24 18:53:59 +0000

    retain dark veil for dark mode alone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.goolgeapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
